548b288808b9780d326154b36166b2c699fedbf2
Dissent-ofc <shivam.khanayat06@gmail.com>
2026-05-20 23:00:19 +0530

    242 changes

--- validAnagram242.java
+++ validAnagram242.java
@@ -1,20 +1,12 @@
-/*Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+/*Given two strings s and t, return true if t is an of s, and false otherwise.
 
 Example 1:
-Input: nums = [1,2,3,1]
+Input: s = "anagram", t = "nagaram"
 Output: true
-Explanation:
-The element 1 occurs at the indices 0 and 3.
 
 Example 2:
-Input: nums = [1,2,3,4]
+Input: s = "rat", t = "car"
 Output: false
-Explanation:
-All elements are distinct.
-
-Example 3:
-Input: nums = [1,1,1,3,3,4,3,2,4,2]
-Output: true
 */
 
 class Solution {
